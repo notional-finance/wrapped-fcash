6990c8a92784fc8d775826c2c864f6fcf87d355e
Jeff Wu <jeff@notional.finance>
2023-09-29 11:09:15 -0700

    fix: changing factory bytecode

--- contracts/tests/testFactory.t.sol
+++ contracts/tests/testFactory.t.sol
@@ -11,6 +11,12 @@ contract TestFactory is BaseTest {
         wrapper = wfCashERC4626(factory.deployWrapper(DAI, maturity_3mo));
     }
 
+    function test_computeAddressMatchesMainnet() public {
+        // Ensure that we get the same address as mainnet
+        assertEq(factory.computeAddress(1, 1702944000),  0x61bc08051000971aCfDE13041Bd701D8F547C645);
+        assertEq(factory.computeAddress(2, 1702944000),  0x5b0aE20561Fcc9e7Eb254cA297869027B75DDd55);
+    }
+
     function test_computeAddress() public {
         address computed = factory.computeAddress(ETH, maturity_3mo);
         vm.expectEmit(true, true, true, true);
